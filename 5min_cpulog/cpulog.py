29a3b657615302438e485a3fbb833204e8dc36b7
jerredcaldwell <76008780+jerredcaldwell@users.noreply.github.com>
2022-04-20 10:30:42 -0500

    Add files via upload

---
+++ 5min_cpulog/cpulog.py
@@ -0,0 +1,17 @@
+import psutil
+from datetime import datetime
+import time
+
+end = time.time() + 60 * 5
+with open('log.txt', 'w') as f:
+
+    while time.time() < end:
+        # time.sleep(1)
+        usage = psutil.cpu_percent(2)
+        usage = str(usage)
+        a = str(datetime.now())
+        f.write(a + '    CPU: ' + usage)
+        f.write('\n')
+        print(usage)
+
+        # print('The CPU usage is: ', psutil.cpu_percent(1))
